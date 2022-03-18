5faf556606668dbb4a14a559f1922c3e67cde468
rradoi <rene.radoi@sva.de>
2022-03-18 15:01:24 +0100

    get database password from secret, create database connection

--- database-table-operator.py
+++ database-table-operator.py
@@ -1,6 +1,10 @@
+"""The database table operator is a demo Python program showing how to handle events on Kubernetes custom resources,
+creating, updating or deleting the respective database table in a local PostgreSQL database."""
+
 import kopf
 import logging
 import kubernetes
+from base64 import b64decode
 import psycopg2
 
 
@@ -8,18 +12,19 @@ import psycopg2
 def create_handler(spec, name, patch, **kwargs):
     logging.info(f"Create handler invoked by custom resource {name}. Creating database table.")
 
+    """Check if spec is valid. Expected items: tableName, columns, primaryKey."""
     table_name = spec.get('tableName')
     if table_name is None:
         raise kopf.PermanentError("Config item 'tableName' is missing in spec.")
-    logging.info(f"table name: {table_name}")
+    logging.debug(f"table name: {table_name}")
 
     table_columns = spec.get('columns')
     if table_columns is None:
         raise kopf.PermanentError("Config item 'columns' is missing in spec.")
-    logging.info(f"table columns: {table_columns}")
+    logging.debug(f"table columns: {table_columns}")
 
     table_keys = spec.get('primaryKey').split()
-    logging.info(f"table primary key: {table_keys}")
+    logging.debug(f"table primary key: {table_keys}")
 
     conn = get_database_connection()
 
@@ -30,9 +35,34 @@ def update_handler(spec, name, diff, patch, **kwargs):
 
 
 @kopf.on.delete('databasetable')
-def delete_handler(name, **kwargs):
+def delete_handler(name, spec, **kwargs):
     logging.info(f"Delete handler invoked by custom resource {name}. Deleting database table.")
+
+    table_name = spec.get('tableName')
+    conn = get_database_connection()
 
 
 def get_database_connection():
-    pass
+    db_name = "demodb"
+    db_user = "demouser"
+    db_host = "localhost"
+    db_port = "5432"
+
+    """"
+    Get database password from Kubernetes secret.
+    If running in a Kubernetes pod, the kube.config has to be loaded using kubernetes.config.load_incluster_config(). 
+    """
+    kubernetes.config.load_config()
+    secret = kubernetes.client.CoreV1Api().read_namespaced_secret('postgresql', 'default').data
+    db_passwd = b64decode(secret['password']).decode('utf-8')
+
+    logging.info(f"database password from secret: {db_passwd}")
+
+    conn = psycopg2.connect(
+        database=db_name,
+        user=db_user,
+        password=db_passwd,
+        host=db_host,
+        port=db_port)
+
+    return conn
